f96c62e50ea8e99309c59cb8d0c28a5f01491a3b
Kelvin <vokemalosh32@gmail.com>
2022-08-21 09:48:59 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,13 +9,3 @@ class Anagram
         array.select {|word| word.chars.sort ==@name.chars.sort}
     end
 end
-# class Anagram
-#     attr_accessor :name
-
-#     def initialize(word)
-#         @name = word 
-#     end 
-# def match array
-#     array.select {|x| x.chars.sort == @name.chars.sort}
-# end
-# end
